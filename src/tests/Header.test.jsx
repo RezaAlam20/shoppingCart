017d35fb15862f59c2446aa59fc3e4cf1275917e
RezaAlam20 <rzaalam384@gmail.com>
2026-05-08 22:06:59 +0330

    fixed test error
    test was crashing due to the changes to Link elements with react router so i added a memoryrouter to fix the make it pass again

--- src/tests/Header.test.jsx
+++ src/tests/Header.test.jsx
@@ -4,24 +4,28 @@ import { screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
 import { describe, it, expect } from "vitest";
+import { MemoryRouter } from "react-router-dom";
 
+function renderWithRouter(component) {
+  return render(<MemoryRouter>{component}</MemoryRouter>);
+}
 describe("Header Ui tests", () => {
   it("checks if header is in document", () => {
-    render(<Header></Header>);
+    renderWithRouter(<Header></Header>);
     screen.debug();
     const header = screen.getByRole("banner");
     expect(header).toBeInTheDocument();
   });
 
   it("checks if buttons in the header are rendered", () => {
-    render(<Header></Header>);
+    renderWithRouter(<Header></Header>);
     const shopBtn = screen.getByText("Shop");
     const CartBtn = screen.getByText("Cart");
     expect(shopBtn).toBeInTheDocument();
     expect(CartBtn).toBeInTheDocument();
   });
   it("checks if the heading title is rendered", () => {
-    render(<Header></Header>);
+    renderWithRouter(<Header></Header>);
     const title = screen.getByText("Fade Out");
     expect(title).toBeInTheDocument();
   });
