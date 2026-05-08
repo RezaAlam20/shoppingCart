401a1e4499e6e4d459e820b0ee18b34f559c6b3e
RezaAlam20 <rzaalam384@gmail.com>
2026-05-08 20:51:42 +0330

    added Data fetching to the Shop component and a loading text

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,3 +1,38 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Item from "./Item";
+
 export default function Shop() {
-  return <></>;
+  const [data, setData] = useState("");
+  const [loading, setloading] = useState("true");
+  useEffect(() => {
+    const getData = async () => {
+      await fetch("https://fakestoreapi.com/products")
+        .then((response) => response.json())
+        .then((data) => {
+          setData(data);
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+        .finally(() => {
+          setloading(false);
+        });
+    };
+    getData();
+  }, []);
+  if (loading) {
+    return <div>Loading ...</div>;
+  }
+  return (
+    <section>
+      <Item
+        url={data[1].image}
+        alt={data[1].description}
+        title={data[1].title}
+        amount={0}
+        price={data[1].price}
+      ></Item>
+    </section>
+  );
 }
