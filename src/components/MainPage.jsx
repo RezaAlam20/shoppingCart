7e41b91245e3d4740d0543951a83d607b6894280
RezaAlam20 <rzaalam384@gmail.com>
2026-05-08 20:51:10 +0330

    MainPage component inital design made

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,7 +1,31 @@
+import img1 from "../assets/Mike.jpg";
+import img2 from "../assets/logan.jpg";
+import img3 from "../assets/Anthony.jpg";
+
+import styles from "../Css/MainPage.module.css";
 export default function MainPage() {
   return (
     <>
-      <h2>Made to fade. Designed to stay</h2>
+      <section>
+        <h1 className={styles.phrase}>Made to fade. Designed to stay</h1>
+
+        <div className={styles.imageContainer}>
+          <img src={img1} alt="image" />
+          <img src={img2} alt="image" />
+          <img src={img3} alt="image" />
+        </div>
+      </section>
+      <section>
+        <h1>Trending</h1>
+        <div className={styles.carousel}>
+          Image carousel
+          <button>add to Cart</button>
+        </div>
+      </section>
+      <section>
+        Shop items
+        <button>view more</button>
+      </section>
     </>
   );
 }
