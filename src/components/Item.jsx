b407be16a1f4275a0704877b7671a410c30482a3
RezaAlam20 <rzaalam384@gmail.com>
2026-05-10 16:40:14 +0330

    basic item component

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,27 +1,38 @@
-export default function Item({ url, alt, title, amount, price }) {
+import styles from "../Css/Item.module.css";
+import { useState } from "react";
+export default function Item({ url, alt, title, price, initialAmount = 0 }) {
+  const [amount, setAmount] = useState(initialAmount);
+  function addToCart() {
+    setAmount(amount + 1);
+  }
+  function removeFromCart() {
+    setAmount(amount - 1);
+  }
   if (amount == 0) {
     return (
-      <div>
+      <div className={styles.item}>
         <img src={url} alt={alt} />
         <div>
           <span>{title}</span>
           <span>{price}</span>
         </div>
-        <button>Add to Cart</button>
+        <button onClick={addToCart}>Add to Cart</button>
       </div>
     );
   } else {
-    <div>
-      <img src={url} alt={alt} />
-      <div>
-        <span>{title}</span>
-        <span>{price}</span>
+    return (
+      <div className={styles.item}>
+        <img src={url} alt={alt} />
+        <div>
+          <span>{title}</span>
+          <span>{price}</span>
+        </div>
+        <div>
+          <button onClick={removeFromCart}>-</button>
+          <span>{amount}</span>
+          <button onClick={addToCart}>+</button>
+        </div>
       </div>
-      <div>
-        <button>-</button>
-        <span>{amount}</span>
-        <button>+</button>
-      </div>
-    </div>;
+    );
   }
 }
