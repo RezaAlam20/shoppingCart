dcafce147801e0528e9d0dbf8c753644ed12ee33
RezaAlam20 <rzaalam384@gmail.com>
2026-05-13 00:35:39 +0330

    put the Link elements in a wrapper and added Classname for styling purposes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,11 @@ export default function Header() {
     <>
       <header className={styles.header}>
         <h2>Fade Out</h2>
-
-        <Link to={"/Shop"}>Shop</Link>
-
-        <Link to={"/Cart"}>Cart</Link>
+        <div className={styles.links}>
+          <Link to={"/"}>Home</Link>
+          <Link to={"/Shop"}>Shop</Link>
+          <Link to={"/Cart"}>Cart</Link>
+        </div>
       </header>
     </>
   );
