3e3b61ba1b846cdc29875ad80ce1ee7e463dba68
RezaAlam20 <rzaalam384@gmail.com>
2026-05-16 01:14:45 +0330

    mocked the outlet context to fix bugs
    since i lifted up the state to pass the data between sibling components and since i was passing it with outletcontext i had to mock it to make the tests work

--- src/tests/Item.test.jsx
+++ src/tests/Item.test.jsx
@@ -1,31 +1,79 @@
-import { findByText, render } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import Item from "../components/Item";
 import { screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { describe, it, expect, vi } from "vitest";
 import userEvent from "@testing-library/user-event";
+import { useOutletContext } from "react-router-dom";
+
+vi.mock("react-router-dom", () => ({
+  useOutletContext: vi.fn(),
+}));
 
 describe("Item Ui tests", () => {
   it("Clicking add to cart should change the amount to 1", async () => {
-    render(<Item></Item>);
+    const mockCart = [];
+    const mockAddToCart = vi.fn();
+    const mockRemoveItem = vi.fn();
+    useOutletContext.mockReturnValue({
+      addToCart: mockAddToCart,
+      cart: mockCart,
+      removeItem: mockRemoveItem,
+    });
+    render(
+      <Item id={1} title="Test" price={100} image={null} alt="" url={null} />,
+    );
+
     const user = userEvent.setup();
+
     await user.click(screen.getByText("Add to Cart"));
-    expect(await screen.findByText("1")).toBeInTheDocument();
+    await user.click(screen.getByText("Add to Cart"));
+
+    expect(mockAddToCart).toHaveBeenCalledTimes(2);
   });
+
   it("Clicking the + button should increment the count", async () => {
-    render(<Item></Item>);
+    const mockCart = [
+      { title: "test", url: "", amount: 1, id: 1, price: 10, alt: "" },
+    ];
+    const mockAddToCart = vi.fn();
+    const mockRemoveItem = vi.fn();
+    useOutletContext.mockReturnValue({
+      addToCart: mockAddToCart,
+      cart: mockCart,
+      removeItem: mockRemoveItem,
+    });
+    render(
+      <Item id={1} title="Test" price={100} image={null} alt="" url={null} />,
+    );
     const user = userEvent.setup();
-    await user.click(screen.getByText("Add to Cart"));
     await user.click(screen.getByText("+"));
     await user.click(screen.getByText("+"));
-    expect(await screen.findByText("3")).toBeInTheDocument();
+    expect(mockAddToCart).toHaveBeenCalled(2);
   });
+
   it("Clicking the - button shuld decrement the item count", async () => {
-    render(<Item></Item>);
+    const mockCart = [
+      { title: "test", url: "", amount: 3, id: 1, price: 10, alt: "" },
+    ];
+    const mockAddToCart = vi.fn();
+    const mockRemoveItem = vi.fn();
+    useOutletContext.mockReturnValue({
+      addToCart: mockAddToCart,
+      cart: mockCart,
+      removeItem: mockRemoveItem,
+    });
+    render(
+      <Item id={1} title="Test" price={100} image={null} alt="" url={null} />,
+    );
+
     const user = userEvent.setup();
-    await user.click(screen.getByText("Add to Cart"));
-    await user.click(screen.getByText("+"));
+
+    expect(screen.getByText("3")).toBeInTheDocument();
+
     await user.click(screen.getByText("-"));
-    expect(await screen.findByText("1")).toBeInTheDocument();
+    await user.click(screen.getByText("-"));
+
+    expect(mockRemoveItem).toHaveBeenCalledTimes(2);
   });
 });
