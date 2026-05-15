ed9579679a0a49460baed9174cae81c111f7ede7
RezaAlam20 <rzaalam384@gmail.com>
2026-05-16 01:17:54 +0330

    mocked the outletcontext of the Api down message

--- src/tests/Shop.test.jsx
+++ src/tests/Shop.test.jsx
@@ -3,12 +3,21 @@ import Shop from "../components/Shop";
 import { screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { describe, it, expect, vi } from "vitest";
+import { useOutletContext } from "react-router-dom";
 
+vi.mock("react-router-dom", () => ({
+  useOutletContext: vi.fn(),
+}));
 describe("Shop Ui tests", () => {
   it("testing the error screen in case the fetch request fails", async () => {
-    window.fetch = vi.fn().mockRejectedValueOnce({ message: "API is down" });
+    useOutletContext.mockReturnValue({
+      data: [],
+      loading: false,
+      error: { message: "API is down" },
+    });
     render(<Shop></Shop>);
     const error = await screen.findByText("API is down");
+    screen.debug();
     expect(error).toBeInTheDocument();
   });
 });
