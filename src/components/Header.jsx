cce0cf14a540681e91a6e0349014871981366e7c
RezaAlam20 <rzaalam384@gmail.com>
2026-05-15 22:12:46 +0330

    added Link to the h2 so that the logo acts as a link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,9 @@ export default function Header() {
   return (
     <>
       <header className={styles.header}>
-        <h2>Fade Out</h2>
+        <h2>
+          <Link to={"/"}>Fade Out</Link>
+        </h2>
         <div className={styles.links}>
           <Link to={"/"} className={styles.link}>
             Home
