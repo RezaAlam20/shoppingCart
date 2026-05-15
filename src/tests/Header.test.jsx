9f7f4dd4e0db743efb31d6f4f25a81c1697f17e8
RezaAlam20 <rzaalam384@gmail.com>
2026-05-16 01:17:18 +0330

    mocked the calcsum function and passed it as a prop to Header component

--- src/tests/Header.test.jsx
+++ src/tests/Header.test.jsx
@@ -3,29 +3,29 @@ import Header from "../components/Header";
 import { screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { MemoryRouter } from "react-router-dom";
 
+const mockCalcSum = vi.fn(() => 0);
 function renderWithRouter(component) {
   return render(<MemoryRouter>{component}</MemoryRouter>);
 }
 describe("Header Ui tests", () => {
   it("checks if header is in document", () => {
-    renderWithRouter(<Header></Header>);
-    screen.debug();
+    renderWithRouter(<Header calcSum={mockCalcSum}></Header>);
     const header = screen.getByRole("banner");
     expect(header).toBeInTheDocument();
   });
 
   it("checks if buttons in the header are rendered", () => {
-    renderWithRouter(<Header></Header>);
+    renderWithRouter(<Header calcSum={mockCalcSum}></Header>);
     const shopBtn = screen.getByText("Shop");
     const CartBtn = screen.getByText("Cart");
     expect(shopBtn).toBeInTheDocument();
     expect(CartBtn).toBeInTheDocument();
   });
   it("checks if the heading title is rendered", () => {
-    renderWithRouter(<Header></Header>);
+    renderWithRouter(<Header calcSum={mockCalcSum}></Header>);
     const title = screen.getByText("Fade Out");
     expect(title).toBeInTheDocument();
   });
